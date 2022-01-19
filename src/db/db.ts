d808ee38270a505907f095f2b871d7ceba889579
Dima <dikashko@gmail.com>
2022-01-19 13:54:41 +0300

    feat: start task, add admin user

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,16 @@
 import 'reflect-metadata';
 import fp from 'fastify-plugin';
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions, getRepository } from 'typeorm';
 import { FastifyInstance } from 'fastify';
 import { User } from './entity/User';
 import { Board } from './entity/Board';
 import { Task } from './entity/Task';
+
+const admin = {
+  name: 'admin',
+  login: 'admin',
+  password: 'admin',
+};
 
 export default fp(async (fastify: FastifyInstance) => {
   try {
@@ -14,6 +20,9 @@ export default fp(async (fastify: FastifyInstance) => {
     });
     await createConnection(connectionOptions);
     fastify.log.info('database connected');
+    const userRepo = getRepository(User);
+    const isAdmin = !!(await userRepo.findOne(admin));
+    if (!isAdmin) await userRepo.save(admin);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
